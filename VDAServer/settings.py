0c0e79f210447f192e1b9456c665e7a297a7046e
shubham <shubhamgawali97@gmail.com>
2020-12-15 10:12:05 +0530

    seventh commit

--- VDAServer/settings.py
+++ VDAServer/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'OPTIONS': {
-            'timeout': 25,
+            'timeout': 50,
         }
     }
 }
