4c390cc6c6aa00e61bef8b18a83400c2e5608231
shubham <shubhamgawali97@gmail.com>
2020-12-16 14:55:48 +0530

    admin pannel added

--- VDAServer/settings.py
+++ VDAServer/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,3 +150,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+mimetypes.add_type("text/css", ".css", True)
